15174a1cd43a34dae7de6d2d221b057cf5295400
Lucas Athayde <athaydaooo@gmail.com>
2025-02-13 18:05:06 -0300

    fix: rollback

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  publicRuntimeConfig: {
-    saudacao: process.env.SAUDACAO || "Oi",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,4 @@
-import getConfig from "next/config";
 import Image from "next/image";
-const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
   return (
@@ -18,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              {`${publicRuntimeConfig.saudacao} LUCAS!`}
+              src/app/page.tsx
             </code>
             .
           </li>
