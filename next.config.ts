1249be65051012dac5e6b86b36e8af4fd8fe62e5
Lucas Athayde <athaydaooo@gmail.com>
2025-02-13 17:59:00 -0300

    fix: env on runtime

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  publicRuntimeConfig: {
+    saudacao: process.env.SAUDACAO || "Oi",
+  },
 };
 
 export default nextConfig;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,6 @@
+import getConfig from "next/config";
 import Image from "next/image";
+const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
   return (
@@ -16,7 +18,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              {`${process.env.SAUDACAO} LUCAS!`}
+              {`${publicRuntimeConfig.saudacao} LUCAS!`}
             </code>
             .
           </li>
