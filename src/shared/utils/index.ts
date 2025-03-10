66c3157e7eb53fefc9dcc468671e52fe7b48811c
Lucas Athayde <athaydaooo@gmail.com>
2025-03-10 11:20:23 -0300

    fix: util funcion fix

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,4 +1,9 @@
-export function dateYearMonth(value: string | number | Date): string {
-  let now = new Date(value).toISOString().split('-') as Array<string>
-  return `${now[0]}-${now[1]}`
+export function dateTimeBR(date: Date): string {
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear().toString()
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const seconds = date.getSeconds().toString().padStart(2, '0')
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
 }
