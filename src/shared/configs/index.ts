1002d1f5a1543c82f57e694697cbb889bf451e0c
Lucas Athayde <athaydaooo@gmail.com>
2025-03-14 10:46:32 -0300

    feat: env config

--- src/shared/configs/index.ts
+++ src/shared/configs/index.ts
@@ -2,6 +2,11 @@ import 'dotenv/config'
 
 const configs = {
   serverPort: process.env.SERVER_PORT || 3000,
+  zabbixApiUrl: process.env.ZABBIX_API_URL || '',
+  zabbixApiUsername: process.env.ZABBIX_API_USERNAME || '',
+  zabbixApiPassword: process.env.ZABBIX_API_PASSWORD || '',
+  zabbixServer: process.env.ZABBIX_SERVER || '',
+  zabbixServerPort: process.env.ZABBIX_SERVER_PORT || '',
 }
 
 export default configs
